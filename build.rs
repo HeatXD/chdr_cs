4b37c3bb34221687bdbacea465e2fb540a7ae1df
HeatXD <45072324+HeatXD@users.noreply.github.com>
2023-04-23 00:51:13 +0200

    Generators all setup

---
+++ build.rs
@@ -0,0 +1,22 @@
+extern crate bindgen;
+extern crate csbindgen;
+
+fn main() {
+    // load chd.h and output rust bindgen to chd.rs
+    bindgen::Builder::default()
+        .clang_args(&["-I./thirdparty/libchdr/include"])
+        .header("thirdparty/libchdr/include/libchdr/chd.h")
+        .generate()
+        .unwrap()
+        .write_to_file("src/chd.rs")
+        .unwrap();
+    // load bindgen code and generate c# code
+    csbindgen::Builder::default()
+        .input_bindgen_file("src/chd.rs")
+        .method_filter(|x| x.starts_with("chd_"))
+        .rust_file_header("use super::chd::*;")
+        .csharp_entry_point_prefix("cs_")
+        .csharp_dll_name("libchdr")
+        .generate_to_file("src/chdr_ffi.rs", "gen/chdr.cs")
+        .unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod chd;
+mod chdr_ffi;
+
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        println!("Hello World!");
     }
 }
