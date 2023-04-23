8ea3d75eb457208c7d461f69a058fc4ed018ed9e
HeatXD <45072324+HeatXD@users.noreply.github.com>
2023-04-24 00:06:49 +0200

    add benchmark example

--- build.rs
+++ build.rs
@@ -4,7 +4,6 @@ extern crate bindgen;
 extern crate csbindgen;
 
 fn main() {
-    println!("cargo:rerun-if-changed=thirdparty/libchdr/include/libchdr/chd.h");
     // load chd.h and output rust bindgen to chd.rs
     bindgen::Builder::default()
         .clang_args(&["-I./thirdparty/libchdr/include"])
@@ -18,8 +17,8 @@ fn main() {
         .input_bindgen_file("src/chd.rs")
         .method_filter(|x| x.starts_with("chd_"))
         .rust_file_header("use super::chd::*;")
-        .csharp_entry_point_prefix("cs_")
-        .csharp_dll_name("libchdr")
+        .csharp_entry_point_prefix("csbindgen_")
+        .csharp_dll_name("libchdr_cs")
         .generate_to_file("src/chdr_ffi.rs", "gen/chdr.cs")
         .unwrap();
     // build the c code with cmake
@@ -32,4 +31,8 @@ fn main() {
         .args(&["--build","gen/build","--config","Release"])
         .status()
         .unwrap();
+
+    println!("cargo:rustc-link-lib=gen/build/deps/lzma-22.01/Release/lzma");     
+    println!("cargo:rustc-link-lib=dylib=/build/deps/zlib-1.2.12/Release/zlib-1");
+    println!("cargo:rustc-link-lib=dylib=gen/build/Release/chdr");
 }
---
+++ example/CHDRTEST/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using CsBindgen;
+
+using NM = CsBindgen.NativeMethods;
+
+namespace CHDRTEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! args: {0}", args.Length != 0 ? string.Join(", ", args) : "none");
+            if (args.Length == 0) return;
+            NativeTest(args[0]);
+        }
+
+        static unsafe void NativeTest(string filepath)
+        {
+
+            Stopwatch stopWatch = new();
+
+            int err;
+
+            _chd_file* file;
+            _chd_header* header;
+
+            IntPtr buffer;
+            uint i;
+            uint totalbytes;
+
+            var path = Encoding.GetEncoding("utf-8").GetBytes(filepath);
+
+            stopWatch.Start();
+
+            fixed (byte* fpath = path)
+            {
+                err = NM.chd_open(fpath, 1, null, &file);
+                string? p = Marshal.PtrToStringUTF8((IntPtr)fpath);
+                Console.WriteLine("path: {0}", p);
+            }
+
+            if (err != 0)
+            {
+                string? error = Marshal.PtrToStringUTF8((IntPtr)NM.chd_error_string(err));
+                Console.WriteLine("Error: {0}", error);
+            }
+
+            header = NM.chd_get_header(file);
+            totalbytes = header->hunkbytes * header->totalhunks;
+            buffer = Marshal.AllocHGlobal((int)header->hunkbytes);
+
+            for (i = 0; i < header->totalhunks; i++)
+            {
+                err = NM.chd_read(file, i, buffer.ToPointer());
+                if (err != 0)
+                {
+                    string? error = Marshal.PtrToStringAuto((IntPtr)NM.chd_error_string(err));
+                    Console.WriteLine("Error: {0}", err);
+                }
+            }
+
+            Marshal.FreeHGlobal(buffer);
+            NM.chd_close(file);
+
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}.{1:000}", ts.Seconds, ts.Milliseconds);
+            Console.WriteLine("RunTime: {0} seconds", elapsedTime);
+            Console.WriteLine("Read: {0} bytes in {1} seconds", totalbytes, elapsedTime);
+            Console.WriteLine("Rate is {0} MB/s", (Convert.ToDouble(totalbytes) / (1024 * 1024)) / ts.TotalSeconds);
+        }
+    }
+}
